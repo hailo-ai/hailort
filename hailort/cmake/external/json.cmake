41ebfdf9cdeea3d5c6f22ca10e63e4b4c0e9f66b
Zoltán Böszörményi <zboszor@gmail.com>
2026-01-07 11:17:35 +0100

    Use a pre-built nlohmann-json dependency
    Signed-off-by: Zoltán Böszörményi <zboszor@gmail.com>

--- hailort/cmake/external/json.cmake
+++ hailort/cmake/external/json.cmake
@@ -1,21 +1,3 @@
 cmake_minimum_required(VERSION 3.11.0)
 
-include(FetchContent)
-
-FetchContent_Declare(
-    json
-    GIT_REPOSITORY https://github.com/ArthurSonzogni/nlohmann_json_cmake_fetchcontent.git
-    GIT_TAG 391786c6c3abdd3eeb993a3154f1f2a4cfe137a0 # Version 3.9.1
-    GIT_SHALLOW TRUE
-    SOURCE_DIR ${HAILO_EXTERNAL_DIR}/json-src
-    SUBBUILD_DIR ${HAILO_EXTERNAL_DIR}/json-subbuild
-)
-
-# https://stackoverflow.com/questions/65527126/disable-install-for-fetchcontent
-FetchContent_GetProperties(json)
-if(NOT json_POPULATED)
-    FetchContent_Populate(json)
-    if (NOT HAILO_EXTERNALS_EXCLUDE_TARGETS)
-        add_subdirectory(${json_SOURCE_DIR} ${json_BINARY_DIR} EXCLUDE_FROM_ALL)
-    endif()
-endif()
+find_package(nlohmann_json REQUIRED CONFIG)
