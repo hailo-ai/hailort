c682c9e2ab3466f78f16231806e8255a115b1f2c
Zoltán Böszörményi <zboszor@gmail.com>
2026-01-07 12:11:30 +0100

    Use a pre-built readerwriterqueue dependency
    Use <readerwriterqueue/readerwriterqueue.h> from the external
    dependency.
    
    Do not link to libreaderwriterqueue, this dependency is
    header-only.
    
    Signed-off-by: Zoltán Böszörményi <zboszor@gmail.com>

--- hailort/cmake/external/readerwriterqueue.cmake
+++ hailort/cmake/external/readerwriterqueue.cmake
@@ -1,23 +1,3 @@
 cmake_minimum_required(VERSION 3.11.0)
 
-include(FetchContent)
-
-FetchContent_Declare(
-    readerwriterqueue
-    GIT_REPOSITORY https://github.com/cameron314/readerwriterqueue
-    GIT_TAG 435e36540e306cac40fcfeab8cc0a22d48464509 # Version 1.0.3
-    GIT_SHALLOW TRUE
-    SOURCE_DIR ${HAILO_EXTERNAL_DIR}/readerwriterqueue-src
-    SUBBUILD_DIR ${HAILO_EXTERNAL_DIR}/readerwriterqueue-subbuild
-)
-
-# https://stackoverflow.com/questions/65527126/disable-install-for-fetchcontent
-FetchContent_GetProperties(readerwriterqueue)
-if(NOT readerwriterqueue_POPULATED)
-    FetchContent_Populate(readerwriterqueue)
-    if (NOT HAILO_EXTERNALS_EXCLUDE_TARGETS)
-        # Add readerwriterqueue as a header-only library
-        add_library(readerwriterqueue INTERFACE)
-        target_include_directories(readerwriterqueue INTERFACE ${readerwriterqueue_SOURCE_DIR})
-    endif()
-endif()
+find_package(readerwriterqueue REQUIRED CONFIG)
